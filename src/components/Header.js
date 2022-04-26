2467272d83bd864285651ed41cafb204a8a1ae8f
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-04-26 15:50:17 +0700

    finished template

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-sm sticky-top navbar-light bg-warning">
+      <div className="container-fluid">
+        <a className="navbar-brand text-black-50 fw-bolder" href="/">
+          EXPENSE TRACKER
+        </a>
+
+        <div className="navbar-collapse justify-content-end">
+          <ul className="navbar-nav gap-x-4">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                <i className="fa-solid fa-home fs-5" />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                <i className="fa-solid fa-plus fs-5" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
